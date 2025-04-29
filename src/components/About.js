00b56c49907be6c20517006020188db99194b9c8
Aysla <ayslaloureiro@gmail.com>
2025-04-29 19:25:28 +0100

    fix

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,14 @@ function About() {
     <div className="About">
       <img src={foto} className="about-img"></img>
       <div className="main-description">
-        <p>My name is Aysla, i am software developer </p>
+        <p className="about-text">
+          Meu nome é Aysla e estou me formando em Desenvolvimento Web pela
+          plataforma TripleTen, onde tive meu primeiro contato com tecnologia.
+          Desde então, aprendi a criar sites, consumir APIs e desenvolver
+          back-ends. Um exemplo do meu progresso é o projeto My Kitchen, uma
+          aplicação React que busca receitas de uma API externa e exibe de forma
+          interativa para o usuário.{" "}
+        </p>
       </div>
     </div>
   );
